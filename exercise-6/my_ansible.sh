99ded3d70406a07ef70a4f1b6acdb5adf1dcf12e
Roman Giovannone-Lean <roman.giovannone-lean@edu.devinci.fr>
2021-12-14 22:32:59 +0100

    Sujet 2 par Roman et Alexis UwU

---
+++ exercise-6/my_ansible.sh
@@ -0,0 +1,13 @@
+mkdir -p documents/gamez
+mkdir -p documents/images
+mkdir -p documents/work/code
+mkdir -p documents/work/plannings
+
+touch -a meeting_notes.txt
+touch -a documents/gamez/csgo.exe
+touch -a documents/work/plannings/january.xlsx
+touch -a documents/work/plannings/february.xlsx
+touch -a documents/work/plannings/march.xlsx
+touch -a documents/work/plannings/april.xlsx
+
+exit 0
